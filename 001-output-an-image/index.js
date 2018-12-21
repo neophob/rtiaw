1f87a1cbfa82e0d98eb08fe48fd2d6b8835054f4
michuNEEO <michael@neeo.com>
2018-12-21 14:53:18 +0100

    example 1

---
+++ 001-output-an-image/index.js
@@ -0,0 +1,28 @@
+console.log('hello world');
+
+c.fillStyle = '#000';
+c.fillRect(0, 0, 800, 600);
+
+const nx = 200;
+const ny = 100;
+let offset = 0;
+
+const image = c.getImageData(0, 0, a.width, a.height);
+
+for (let j = ny - 1; j >= 0; j--) {
+  for (let i = 0; i < nx; i++) {
+    const r = i / nx;
+    const g = j / ny;
+    const b = 0.2;
+
+    image.data[offset    ] = 255.99 * r;
+    image.data[offset + 1] = 255.99 * g;
+    image.data[offset + 2] = 255.99 * b;
+    image.data[offset + 3] = 255;
+
+    offset += 4;
+  }
+  offset += 4 * (a.width - nx);
+}
+c.putImageData(image, 0, 0);
+console.log('done');
